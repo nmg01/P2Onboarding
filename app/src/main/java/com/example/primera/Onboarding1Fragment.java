da7ed0b3824b16a95ed3c22db9051fa4b1c04a7e
dam2a-2021 <nmoyag01@elpuig.xeill.net>
2021-09-30 16:54:50 +0200

    f

---
+++ app/src/main/java/com/example/primera/Onboarding1Fragment.java
@@ -0,0 +1,42 @@
+package com.example.primera;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Onboarding1Fragment extends Fragment {
+
+    Button botonSiguiente;
+    NavController navController;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_onboarding2, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        botonSiguiente = view.findViewById(R.id.botonSiguiente);
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment);
+            }
+        });
+    }
+}
